cfa9073cf37d9c15420b3e7f051150d522ffc8fc
je.rnic <yeojernic@gmail.com>
2024-04-03 16:14:01 +0800

    Revert "Merge branch 'main' of https://github.com/WaiHongL/ESD"
    This reverts commit 807e4b60cb151dfe0597d072af83bd31102788fa, reversing
    changes made to 0372ed252ca221053f74ff5bb1797b308bbd999f.

--- Backend/recommend.py
+++ Backend/recommend.py
@@ -72,9 +72,7 @@ def get_common_genre():
 
         # CHECKS IF GENRE_DATA IS EMPTY
         try:
-            max_value = max(genre_dict.values())
-
-            common_genre = [k for k,v in genre_dict.items() if v == max_value]
+            common_genre = max(genre_dict, key=genre_dict.get)
             return jsonify(
                 {
                     "code": 200,
